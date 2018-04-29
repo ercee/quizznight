a9aab372169035eedd9b3efe0968455708814523
ercee <ercetilav@gmail.com>
2018-04-29 11:59:11 +0300

    Initial commit

---
+++ src/test/java/pub/games/quizznight/QuizznightApplicationTests.java
@@ -0,0 +1,16 @@
+package pub.games.quizznight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizznightApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
